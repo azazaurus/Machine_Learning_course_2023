2bccaf005aead94310dfcfd3240b52b004cc587f
azazaurus <1maxovmax!@gmail.com>
2023-12-19 12:47:03 +0300

    wip

---
+++ pythonProject2/main.py
@@ -0,0 +1,222 @@
+import math
+
+import cv2
+import keras
+import numpy as np
+from scipy.ndimage.measurements import center_of_mass
+from keras.datasets import mnist
+from keras.layers import Conv2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers import MaxPooling2D
+from keras.models import Sequential
+from keras.optimizers import SGD
+from keras.utils import to_categorical
+
+
+def main():
+    model = run_test_harness()
+    print(predict(model, "-12.png"))
+
+
+# load train and test dataset
+def load_dataset():
+    # load dataset
+    (trainX, trainY), (testX, testY) = mnist.load_data()
+    # reshape dataset to have a single channel
+    trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+    testX = testX.reshape((testX.shape[0], 28, 28, 1))
+    # one hot encode target values
+    trainY = to_categorical(trainY)
+    testY = to_categorical(testY)
+    return trainX, trainY, testX, testY
+
+
+# scale pixels
+def prep_pixels(train, test):
+    # convert from integers to floats
+    train_norm = train.astype('float32')
+    test_norm = test.astype('float32')
+    # normalize to range 0-1
+    train_norm = train_norm / 255.0
+    test_norm = test_norm / 255.0
+    # return normalized images
+    return train_norm, test_norm
+
+
+# define cnn model
+def define_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(10, activation='softmax'))
+    # compile model
+    opt = SGD(learning_rate=0.01, momentum=0.9)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
+# run the test harness for evaluating a model
+def run_test_harness():
+    # load dataset
+    trainX, trainY, testX, testY = load_dataset()
+    # prepare pixel data
+    trainX, testX = prep_pixels(trainX, testX)
+    # define model
+    model = define_model()
+    # fit model
+    # model.fit(trainX, trainY, epochs=10, batch_size=32, verbose=1)
+    # # save model
+    # model.save('digit_model.h5')
+    model = keras.models.load_model('digit_model.h5')
+    _, acc = model.evaluate(testX, testY, verbose=0)
+    print('> %.3f' % (acc * 100.0))
+    return model
+
+
+def letters_extract(image_file: str, out_size=28):
+    img = cv2.imread(image_file)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
+    img_erode = cv2.erode(thresh, np.ones((3, 3), np.uint8), iterations=1)
+
+    # Get contours
+    contours, hierarchy = cv2.findContours(img_erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    output = img.copy()
+
+    letters = []
+    for idx, contour in enumerate(contours):
+        (x, y, w, h) = cv2.boundingRect(contour)
+        # print("R", idx, x, y, w, h, cv2.contourArea(contour), hierarchy[0][idx])
+        # hierarchy[i][0]: the index of the next contour of the same level
+        # hierarchy[i][1]: the index of the previous contour of the same level
+        # hierarchy[i][2]: the index of the first child
+        # hierarchy[i][3]: the index of the parent
+        if hierarchy[0][idx][3] == 0:
+            cv2.rectangle(output, (x, y), (x + w, y + h), (70, 0, 0), 1)
+            letter_crop = gray[y:y + h, x:x + w]
+            # print(letter_crop.shape)
+
+            # Resize letter canvas to square
+            size_max = max(w, h)
+            letter_square = 255 * np.ones(shape=[size_max, size_max], dtype=np.uint8)
+            if w > h:
+                # Enlarge image top-bottom
+                # ------
+                # ======
+                # ------
+                y_pos = size_max // 2 - h // 2
+                letter_square[y_pos:y_pos + h, 0:w] = letter_crop
+            elif w < h:
+                # Enlarge image left-right
+                # --||--
+                x_pos = size_max // 2 - w // 2
+                letter_square[0:h, x_pos:x_pos + w] = letter_crop
+            else:
+                letter_square = letter_crop
+
+            # Resize letter to 28x28 and add letter and its X-coordinate
+            letters.append((x, w, cv2.resize(letter_square, (out_size, out_size), interpolation=cv2.INTER_AREA)))
+
+    # Sort array in place by X-coordinate
+    letters.sort(key=lambda x: x[0], reverse=False)
+
+    # cv2.imshow("Input", img)
+    # cv2.imshow("Gray", thresh)
+    # cv2.imshow("Enlarged", img_erode)
+    # cv2.imshow("Output", output)
+    # for i in range(len(letters)):
+    #     cv2.imshow(str(i), letters[i][2])
+    #
+    # cv2.waitKey(0)
+    return letters
+
+
+def getBestShift(img):
+    cy, cx = center_of_mass(img)
+
+    rows, cols = img.shape
+    shiftx = np.round(cols / 2.0 - cx).astype(int)
+    shifty = np.round(rows / 2.0 - cy).astype(int)
+
+    return shiftx, shifty
+
+
+def shift(img, sx, sy):
+    rows, cols = img.shape
+    M = np.float32([[1, 0, sx], [0, 1, sy]])
+    shifted = cv2.warpAffine(img, M, (cols, rows))
+    return shifted
+
+
+def rec_digit(img_path, model):
+    img = img_path
+    gray = 255 - img
+    # применяем пороговую обработку
+    (thresh, gray) = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+    # удаляем нулевые строки и столбцы
+    while np.sum(gray[0]) == 0:
+        gray = gray[1:]
+    while np.sum(gray[:, 0]) == 0:
+        gray = np.delete(gray, 0, 1)
+    while np.sum(gray[-1]) == 0:
+        gray = gray[:-1]
+    while np.sum(gray[:, -1]) == 0:
+        gray = np.delete(gray, -1, 1)
+    rows, cols = gray.shape
+
+    # изменяем размер, чтобы помещалось в box 20x20 пикселей
+    if rows > cols:
+        factor = 20.0 / rows
+        rows = 20
+        cols = int(round(cols * factor))
+        gray = cv2.resize(gray, (cols, rows))
+    else:
+        factor = 20.0 / cols
+        cols = 20
+        rows = int(round(rows * factor))
+        gray = cv2.resize(gray, (cols, rows))
+
+    # расширяем до размера 28x28
+    colsPadding = (int(math.ceil((28 - cols) / 2.0)), int(math.floor((28 - cols) / 2.0)))
+    rowsPadding = (int(math.ceil((28 - rows) / 2.0)), int(math.floor((28 - rows) / 2.0)))
+    gray = np.lib.pad(gray, (rowsPadding, colsPadding), 'constant')
+
+    # сдвигаем центр масс
+    shiftx, shifty = getBestShift(gray)
+    shifted = shift(gray, shiftx, shifty)
+    gray = shifted
+
+    # cv2.imwrite('gray' + img_path, gray)
+    img = gray / 255.0
+    img = np.array(img).reshape(-1, 28, 28, 1)
+    out = str(np.argmax(model.predict(img)))
+    return out
+
+
+def show_images(images: []):
+    for i in range(len(images)):
+        cv2.imshow(str(i), images[i][2])
+
+    cv2.waitKey(0)
+
+
+def predict(model, image: str):
+    letters = letters_extract(image)
+    result = ""
+
+    for i in range(len(letters)):
+        result += rec_digit(letters[i][2], model)
+
+    return int(result)
+
+
+if __name__ == "__main__":
+    main()
